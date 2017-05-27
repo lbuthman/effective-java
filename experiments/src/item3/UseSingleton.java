d5443480bf53a06c64bce430b5b6899fcab64db8
lbuthman <lbuthman@gmail.com>
2017-05-27 09:06:24 -0500

    Add example of singleton enum type instantiation

---
+++ experiments/src/item3/UseSingleton.java
@@ -0,0 +1,18 @@
+package item3;
+
+/**
+ * Created by lbuthman on 5/27/17.
+ */
+enum EnumSingleton {
+    INSTANCE;
+
+    public void lovingIt() {System.out.println("Nope! Single and lovin' it!");}
+}
+
+public class UseSingleton {
+    public static void main(String[] args) {
+        System.out.println("Hi Singleton, have you met anyone?");
+        EnumSingleton single = EnumSingleton.INSTANCE;
+        single.lovingIt();
+    }
+}
